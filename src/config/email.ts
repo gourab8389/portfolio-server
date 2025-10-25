c41e3bc0479209f15a89c907347b44b42f4ab32d
gourab8389 <deyg6988@gmail.com>
2025-10-25 23:50:20 +0530

    feat: integrate Resend for email handling and update dependencies in package.json and package-lock.json

--- src/config/email.ts
+++ src/config/email.ts
@@ -1,21 +1,25 @@
-import nodemailer from 'nodemailer';
+// import nodemailer from 'nodemailer';
+import { Resend } from "resend";
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-export const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: parseInt(process.env.SMTP_PORT || '587'),
-  secure: false,
-  auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
-  },
-});
+export const resend = new Resend(process.env.RESEND_API_KEY);
+
+
+// export const transporter = nodemailer.createTransport({
+//   host: process.env.SMTP_HOST,
+//   port: parseInt(process.env.SMTP_PORT || '587'),
+//   secure: false,
+//   auth: {
+//     user: process.env.SMTP_USER,
+//     pass: process.env.SMTP_PASS,
+//   },
+// });
 
 export const emailConfig = {
   from: {
-    name: process.env.FROM_NAME || 'Gourab',
+    name: process.env.FROM_NAME || 'Gourab Dey',
     address: process.env.FROM_EMAIL || '',
   },
 };
